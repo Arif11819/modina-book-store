3ccb011c39107781f89e8e1961b0b9baf6d70720
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-03-26 11:54:42 +0600

    added file after cart name added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart }) => {
+
     return (
-        <div>
-            <h1>Selected Books</h1>
-            <p> item: {cart.length}</p>
-        </div>
+        <div className='cart'>
+            <h1> Selected Books</h1>
+            {
+                cart.map(ct => <p className='cart-name'>{ct.name}</p>)
+            }
+            <button className='choose-btn'>
+                <p className='btn-text'>CHOOSE 1 FOR ME</p>
+            </button>
+
+        </div >
     );
 };
 
